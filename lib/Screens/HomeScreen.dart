6523fcc9a18d7151c9cbc4a742f153ca6f19a033
Taranpreet Singh <taran99.singh@gmail.com>
2020-09-29 16:58:10 -0400

    Initial publish

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  Widget build(BuildContext context){
+    
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'Screens/HomeScreen.dart';
+import 'Screens/SignInScreen.dart';
+import 'Screens/SignUPScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'PetAssist: Sign up',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal
+      ),
+      initialRoute: 'SignIn',
+      routes: {
+        'SignIn':(context)=>SignInScreen(),
+        'SignUp': (context)=> SignUpScreen(),
+        'Home': (context)=> Homescreen(),
+      },
+    );
+  }
+}
